e973a4c596508c795071791355cabf30814b33e4
Alexandr Molodtsov <molodtsov@medipal.tech>
2026-07-19 10:18:05 +0300

    added todo comment plugin

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,6 +1,14 @@
 -- KEYBINDS
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+    require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
 vim.keymap.set("n", "<leader>df", ":lua require('neogen').generate()<CR>", { silent = true })
 
 vim.keymap.set("i", "<C-t>", "{%  %}<Left><Left><Left>")
